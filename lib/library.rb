3311925844c89c44ba13af929756b70901308e5e
Leah Miller <lkmiller.87@gmail.com>
2018-11-05 21:19:51 -0700

    Adds passing tests for iteration 3

--- lib/library.rb
+++ lib/library.rb
@@ -1,5 +1,10 @@
 class Library
+  attr_reader :books
   def initialize
+    @books = []
+  end
 
+  def add_to_collection(book_instance)
+    @books << book_instance
   end
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -1,6 +1,7 @@
 require './test/test_helper'
 require './lib/library'
 require './lib/author'
+require './lib/book'
 
 class LibraryTest < Minitest::Test
   def test_it_exists
@@ -13,6 +14,14 @@ class LibraryTest < Minitest::Test
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     jane_eyre = charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
     villette  = charlotte_bronte.add_book("Villette", "1853")
+    harper_lee  = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.add_book("To Kill a Mockingbird", "July 11, 1960")
+    dpl.books
+    dpl.add_to_collection(jane_eyre)
+    assert_equal [jane_eyre], dpl.books
+    dpl.add_to_collection(mockingbird)
+    dpl.add_to_collection(villette)
+    assert_equal 3, dpl.books.count
   end
 
 end
